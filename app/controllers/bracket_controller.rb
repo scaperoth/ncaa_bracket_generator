b432b8b816a212ca23ac23e0d18d3fc5fae497f0
scaperoth <scaperoth@gmail.com>
2017-03-11 11:21:08 -0500

    fixing bug in comparison

--- app/controllers/bracket_controller.rb
+++ app/controllers/bracket_controller.rb
@@ -1,30 +1,29 @@
 class BracketController < ApplicationController
-  before_filter :set_tournament_and_method
-  
-  def index
-    games = BracketGame.where(tournament: @tournament)
-    if games.empty?
-      render "coming_soon"
+    before_filter :set_tournament_and_method
+
+    def index
+        games = BracketGame.where(tournament: @tournament)
+        render 'coming_soon' if games.empty?
     end
-  end
-  
-  def comparison
-  end
-  
-  private
-  
-  def set_tournament_and_method
-    if(params['method'].size == 2)
-      params[:method] = "all"
-    else
-      params[:method] = params['method'].keys.first
+
+    def comparison
     end
-    
-    tournament_year = params[:year] || Time.now.year.to_s
-    @tournament = Tournament.find_by year: tournament_year
-    
-    @bracket_helper = BracketHelper 
-    @method = params[:method] || "all"
-    
-  end
+
+    private
+
+    def set_tournament_and_method
+        unless params['method'].nil?
+            params[:method] = if params['method'].size == 2
+                                  'all'
+                              else
+                                  params['method'].keys.first
+                              end
+        end
+
+        tournament_year = params[:year] || Time.now.year.to_s
+        @tournament = Tournament.find_by year: tournament_year
+
+        @bracket_helper = BracketHelper
+        @method = params[:method] || 'all'
+      end
 end
