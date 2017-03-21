98e4a7771dbeea758673da1ed087f2b6b539e866
scaperoth <scaperoth@gmail.com>
2017-03-21 09:47:39 -0400

    updated tournament matches with new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,5 +98,6 @@ bracket_games = ActiveSupport::JSON.decode(File.read('db/seed_files/tournament_m
 bracket_games.each do |game|
     BracketGame.create(tournament_id: game['tournament_id'], round_id: game['round_id'],
                        region_id: game['region_id'], team_id: game['team_id'], team2_id: game['team2_id'],
+                       team1_score: game['team1_score'], team2_score: game['team2_score'],
                        weight: game['weight'], winner_id: game['winner_id'])
 end
